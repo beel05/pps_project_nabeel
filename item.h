df3713b0acf9809ec1d88af36bb06d1d122edb96
Prateek <25LBCS3093@culkomail.in>
2025-11-29 15:59:52 +0530

    Update item.h
    code change

--- item.h
+++ item.h
@@ -4,17 +4,18 @@
 
 typedef struct {
     char name[50];
-    int days;           // number of historical days
-    double *sales;      // dynamic array for daily sales
-    double leadTime;    // lead time in days
+    int days;          
+    double *sales;      
+double leadTime;    
 } Item;
 
-// Forecasting functions
+
 double moving_average(double *sales, int n, int window);
 double weighted_moving_average(double *sales, int n, int window);
 
-// Statistics functions
+
 double compute_mape(double *actual, double *forecast, int n);
 double compute_stddev(double *sales, int n);
 
 #endif
+
